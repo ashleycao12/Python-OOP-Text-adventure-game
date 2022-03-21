97f59b727b8563a75766523aa25df30c37e82c32
Ashley Cao <ashleycao12@gmail.com>
2022-03-21 16:12:15 +1300

    Change object design

--- Main.py
+++ Main.py
@@ -1,43 +1,46 @@
 import json
 
-# from numpy import False_
-
-Stage_file = open('Stages3.json')
+Stage_file = open('Stages.json')
 Stage_data = json.load(Stage_file)
 
 class Stage:
     def __init__(self, Stage_key):
         Stage_obj = Stage_data[Stage_key]
         self.Text = Stage_obj["Text"]
-        self.childStages = Stage_obj["ChildStages"]
-        if self.childStages ==[]:
+        self.options = Stage_obj["Options"]
+        if self.options ==[]:
             self.isEnding = True
         else: self.isEnding = False
 
     def GetChildStage(self, child_Index):
-        childStage_name = self.childStages[child_Index -1]
+        childStage_name = self.options[child_Index -1]["ChildStage"]
         childStage = Stage(childStage_name)
         return childStage
 
 def inputValid(input,Stage):
     try: 
         input = int(input)
-        if input > len(Stage.childStages):
+        if input > len(Stage.options):
             return False
         else:
             return True
     except ValueError:
         return False
+def printStory(stage):
+    print(stage.Text)
+    for option in stage.options:
+        print(option["OptionText"])
+    
 
 currentStage = Stage("Stage0")
-print(currentStage.Text)
+printStory(currentStage)
 while currentStage.isEnding == False:
     choice_txt = input("enter your choice:")
     while inputValid(choice_txt,currentStage) == False:
         choice_txt = input("enter a valid choice:")
     choice_int = int(choice_txt)
     currentStage = currentStage.GetChildStage(choice_int)
-    print(currentStage.Text)
+    printStory(currentStage)
 print ('the end')
 
 
